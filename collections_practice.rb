f5e269163f7e89eee12b0a55906efdec1bbd13f0
Owen Cruse <ocruse7@gmail.com>
2019-11-15 05:57:26 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
 
+def sort_array_asc( array )
+  array.sort
+end
+
+def sort_array_desc( array )
+  array.sort do |a, b| b <=> a
+ end
+end
+
+def sort_array_char_count( array )
+  array.sort do |a, b| a.length <=> b.length 
+end
+end
+
+def swap_elements( array )
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array( num )
+  num.reverse
+end
+
+def kesha_maker( array )
+  array.each {|string| string[2] = "$"}
+  
+end
+
+def find_a( array )  
+  array.find_all {|string| string.start_with? ("a")}
+end
+
+def sum_array( array )
+  array.inject {|a,b| a + b }
+end
+
+def add_s(array)
+  new = []
+  new << array[0] + "s"
+  new << array[1]
+  new << array[2] + "s"
+  new << array[3] + "s"
+  new
+end
